dddacca43d7965adfe04b137a85a8800c2f44b23
Gorki <115663070+SpiritLeader1@users.noreply.github.com>
2025-09-17 00:00:54 +0300

    [UPDATE] New Images Added

---
+++ README.md
@@ -0,0 +1,6 @@
+# POMS Images
+
+## Güncelleme Notları
+
+* Her kategorinin görsel sayısı 30'a çıkarıldı.
+* Her bir görsel 10000 ms gösterilecek olup kategoriler 5 dakika sürecek şekilde ayarlanmıştır.
--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const photos = {
     anger: [
+        "Emotional Pictures (Selected)/Anger/1713211999287.png",
+        "Emotional Pictures (Selected)/Anger/1713211999294.png",
+        "Emotional Pictures (Selected)/Anger/1713211999299.png",
         "Emotional Pictures (Selected)/Anger/2683.jpg",
         "Emotional Pictures (Selected)/Anger/2691.jpg",
         "Emotional Pictures (Selected)/Anger/6211.jpg",
@@ -8,22 +11,60 @@ const photos = {
         "Emotional Pictures (Selected)/Anger/6520.jpg",
         "Emotional Pictures (Selected)/Anger/6540.jpg",
         "Emotional Pictures (Selected)/Anger/6550.jpg",
-        "Emotional Pictures (Selected)/Anger/6560.jpg",
-        "Emotional Pictures (Selected)/Anger/9163.jpg"
+        "Emotional Pictures (Selected)/Anger/6560.JPG",
+        "Emotional Pictures (Selected)/Anger/9163.jpg",
+        "Emotional Pictures (Selected)/Anger/Angry pose 2.jpg",
+        "Emotional Pictures (Selected)/Anger/Bloody knife 1.jpg",
+        "Emotional Pictures (Selected)/Anger/Gun 9.jpg",
+        "Emotional Pictures (Selected)/Anger/gun.jpg",
+        "Emotional Pictures (Selected)/Anger/img-fSc1FimtErImg0JE2Hd07.png",
+        "Emotional Pictures (Selected)/Anger/img-HLXiUlWQqm0WAOD7l8vIs.jpeg",
+        "Emotional Pictures (Selected)/Anger/img-hX4TYQRTfCWYAcgoThT4N.png",
+        "Emotional Pictures (Selected)/Anger/img-lGOoHz4LCOuaNdcXuBWdx.jpeg",
+        "Emotional Pictures (Selected)/Anger/img-NloyUUcOPwLgiBWzFzLJA.png",
+        "Emotional Pictures (Selected)/Anger/img-PfYm5T1VSd2w35rPSajZb.png",
+        "Emotional Pictures (Selected)/Anger/img-qvI5hRTqa6wDUGr13YUJa.png",
+        "Emotional Pictures (Selected)/Anger/img-seGqsG6h2mFs8ttHjKhhW.png",
+        "Emotional Pictures (Selected)/Anger/img-v7FCzrQUrNUAVq7VrS1Ws.png",
+        "Emotional Pictures (Selected)/Anger/img-yryUB6V1CU4arLfRDDRWN.png",
+        "Emotional Pictures (Selected)/Anger/Police 1.jpg",
+        "Emotional Pictures (Selected)/Anger/War 1.jpg",
+        "Emotional Pictures (Selected)/Anger/War 6.jpg"
     ],
     confusion: [
+        "Emotional Pictures (Selected)/Confusion/7061.jpg",
+        "Emotional Pictures (Selected)/Confusion/7095.jpg",
+        "Emotional Pictures (Selected)/Confusion/7096.jpg",
+        "Emotional Pictures (Selected)/Confusion/7160.jpg",
+        "Emotional Pictures (Selected)/Confusion/7179.jpg",
+        "Emotional Pictures (Selected)/Confusion/7182.jpg",
+        "Emotional Pictures (Selected)/Confusion/7183.jpg",
         "Emotional Pictures (Selected)/Confusion/7184.jpg",
+        "Emotional Pictures (Selected)/Confusion/7185.jpg",
+        "Emotional Pictures (Selected)/Confusion/7186.jpg",
         "Emotional Pictures (Selected)/Confusion/7188.jpg",
+        "Emotional Pictures (Selected)/Confusion/7192.jpg",
+        "Emotional Pictures (Selected)/Confusion/7195.jpg",
+        "Emotional Pictures (Selected)/Confusion/7205.jpg",
         "Emotional Pictures (Selected)/Confusion/7207.jpg",
         "Emotional Pictures (Selected)/Confusion/7237.jpg",
+        "Emotional Pictures (Selected)/Confusion/7238.jpg",
+        "Emotional Pictures (Selected)/Confusion/7240.jpg",
         "Emotional Pictures (Selected)/Confusion/7242.jpg",
         "Emotional Pictures (Selected)/Confusion/7247.jpg",
         "Emotional Pictures (Selected)/Confusion/7248.jpg",
         "Emotional Pictures (Selected)/Confusion/7249.jpg",
         "Emotional Pictures (Selected)/Confusion/7497.jpg",
-        "Emotional Pictures (Selected)/Confusion/9468.jpg"
+        "Emotional Pictures (Selected)/Confusion/9468.jpg",
+        "Emotional Pictures (Selected)/Confusion/Building 1.jpg",
+        "Emotional Pictures (Selected)/Confusion/Galaxy 3.jpg",
+        "Emotional Pictures (Selected)/Confusion/Keys 1.jpg",
+        "Emotional Pictures (Selected)/Confusion/Storage 1.jpg",
+        "Emotional Pictures (Selected)/Confusion/Storage 2.jpg",
+        "Emotional Pictures (Selected)/Confusion/Wedding 12.jpg"
     ],
     depression: [
+        "Emotional Pictures (Selected)/Depression/1713282491938.jpeg",
         "Emotional Pictures (Selected)/Depression/2205.jpg",
         "Emotional Pictures (Selected)/Depression/2276.jpg",
         "Emotional Pictures (Selected)/Depression/2301.jpg",
@@ -33,7 +74,26 @@ const photos = {
         "Emotional Pictures (Selected)/Depression/3300.jpg",
         "Emotional Pictures (Selected)/Depression/6311.jpg",
         "Emotional Pictures (Selected)/Depression/9041.jpg",
-        "Emotional Pictures (Selected)/Depression/9432.jpg"
+        "Emotional Pictures (Selected)/Depression/9432.jpg",
+        "Emotional Pictures (Selected)/Depression/Frustrated pose 5.jpg",
+        "Emotional Pictures (Selected)/Depression/H005.bmp",
+        "Emotional Pictures (Selected)/Depression/H034.bmp",
+        "Emotional Pictures (Selected)/Depression/H063.bmp",
+        "Emotional Pictures (Selected)/Depression/H077.bmp",
+        "Emotional Pictures (Selected)/Depression/H078.bmp",
+        "Emotional Pictures (Selected)/Depression/H099.bmp",
+        "Emotional Pictures (Selected)/Depression/H105.bmp",
+        "Emotional Pictures (Selected)/Depression/H112.bmp",
+        "Emotional Pictures (Selected)/Depression/img-e7m38HY25gIpaQFLZA9gt.png",
+        "Emotional Pictures (Selected)/Depression/img-PLl8wQ09NsYwFaCfF646P.png",
+        "Emotional Pictures (Selected)/Depression/img-tkxmgwPTjrxFfQXLoHkEv.png",
+        "Emotional Pictures (Selected)/Depression/img-WPZGFFAHpjGWQGwoA0Iij.png",
+        "Emotional Pictures (Selected)/Depression/img-zyWIhO0T4CJJnCryN3XXn.png",
+        "Emotional Pictures (Selected)/Depression/Miserable face 2.jpg",
+        "Emotional Pictures (Selected)/Depression/Miserable pose 3.jpg",
+        "Emotional Pictures (Selected)/Depression/Sad pose 1.jpg",
+        "Emotional Pictures (Selected)/Depression/Sad pose 3.jpg",
+        "Emotional Pictures (Selected)/Depression/Sad pose 7.jpg"
     ],
     fatigue: [
         "Emotional Pictures (Selected)/Fatigue/2393.jpg",
@@ -45,7 +105,27 @@ const photos = {
         "Emotional Pictures (Selected)/Fatigue/7060.jpg",
         "Emotional Pictures (Selected)/Fatigue/7224.jpg",
         "Emotional Pictures (Selected)/Fatigue/7700.jpg",
-        "Emotional Pictures (Selected)/Fatigue/9360.jpg"
+        "Emotional Pictures (Selected)/Fatigue/9360.jpg",
+        "Emotional Pictures (Selected)/Fatigue/Bored pose 1.jpg",
+        "Emotional Pictures (Selected)/Fatigue/Bored pose 2.jpg",
+        "Emotional Pictures (Selected)/Fatigue/Bored pose 3.jpg",
+        "Emotional Pictures (Selected)/Fatigue/Bored pose 4.jpg",
+        "Emotional Pictures (Selected)/Fatigue/Cardboard 3.jpg",
+        "Emotional Pictures (Selected)/Fatigue/Dirt 5.jpg",
+        "Emotional Pictures (Selected)/Fatigue/drunk.jpg",
+        "Emotional Pictures (Selected)/Fatigue/Fence 5.jpg",
+        "Emotional Pictures (Selected)/Fatigue/N002.bmp",
+        "Emotional Pictures (Selected)/Fatigue/N011.bmp",
+        "Emotional Pictures (Selected)/Fatigue/N025.bmp",
+        "Emotional Pictures (Selected)/Fatigue/N026.bmp",
+        "Emotional Pictures (Selected)/Fatigue/N039.bmp",
+        "Emotional Pictures (Selected)/Fatigue/N046.bmp",
+        "Emotional Pictures (Selected)/Fatigue/N064.bmp",
+        "Emotional Pictures (Selected)/Fatigue/N066.bmp",
+        "Emotional Pictures (Selected)/Fatigue/N075.bmp",
+        "Emotional Pictures (Selected)/Fatigue/N097.bmp",
+        "Emotional Pictures (Selected)/Fatigue/Sleepy pose 2.jpg",
+        "Emotional Pictures (Selected)/Fatigue/Sleepy pose 3.jpg"
     ],
     tension: [
         "Emotional Pictures (Selected)/Tension/2730.jpg",
@@ -57,7 +137,27 @@ const photos = {
         "Emotional Pictures (Selected)/Tension/3400.jpg",
         "Emotional Pictures (Selected)/Tension/9252.jpg",
         "Emotional Pictures (Selected)/Tension/9325.jpg",
-        "Emotional Pictures (Selected)/Tension/9405.jpg"
+        "Emotional Pictures (Selected)/Tension/9405.jpg",
+        "Emotional Pictures (Selected)/Tension/A001.bmp",
+        "Emotional Pictures (Selected)/Tension/A008.bmp",
+        "Emotional Pictures (Selected)/Tension/A029.bmp",
+        "Emotional Pictures (Selected)/Tension/A041.bmp",
+        "Emotional Pictures (Selected)/Tension/A068.bmp",
+        "Emotional Pictures (Selected)/Tension/A095.bmp",
+        "Emotional Pictures (Selected)/Tension/Animal carcass 6.jpg",
+        "Emotional Pictures (Selected)/Tension/Dead bodies 1.jpg",
+        "Emotional Pictures (Selected)/Tension/Dead bodies 3.jpg",
+        "Emotional Pictures (Selected)/Tension/Dog attack 3.jpg",
+        "Emotional Pictures (Selected)/Tension/Dummy 1.jpg",
+        "Emotional Pictures (Selected)/Tension/Injury 3.jpg",
+        "Emotional Pictures (Selected)/Tension/Injury 4.jpg",
+        "Emotional Pictures (Selected)/Tension/Severed finger 1.jpg",
+        "Emotional Pictures (Selected)/Tension/Shot 1.jpg",
+        "Emotional Pictures (Selected)/Tension/Shot 3.jpg",
+        "Emotional Pictures (Selected)/Tension/Snake 4.jpg",
+        "Emotional Pictures (Selected)/Tension/Sp051.bmp",
+        "Emotional Pictures (Selected)/Tension/Spider 2.jpg",
+        "Emotional Pictures (Selected)/Tension/Tumor 1.jpg"
     ],
     vigor: [
         "Emotional Pictures (Selected)/Vigor/1710.jpg",
@@ -69,14 +169,34 @@ const photos = {
         "Emotional Pictures (Selected)/Vigor/4601.jpg",
         "Emotional Pictures (Selected)/Vigor/4626.jpg",
         "Emotional Pictures (Selected)/Vigor/8033.jpg",
-        "Emotional Pictures (Selected)/Vigor/8490.jpg"
+        "Emotional Pictures (Selected)/Vigor/8490.jpg",
+        "Emotional Pictures (Selected)/Vigor/Nude couple 4.jpg",
+        "Emotional Pictures (Selected)/Vigor/P011.bmp",
+        "Emotional Pictures (Selected)/Vigor/P013.bmp",
+        "Emotional Pictures (Selected)/Vigor/P022.bmp",
+        "Emotional Pictures (Selected)/Vigor/P028.bmp",
+        "Emotional Pictures (Selected)/Vigor/P033.bmp",
+        "Emotional Pictures (Selected)/Vigor/P035.bmp",
+        "Emotional Pictures (Selected)/Vigor/P037.bmp",
+        "Emotional Pictures (Selected)/Vigor/P038.bmp",
+        "Emotional Pictures (Selected)/Vigor/P042.bmp",
+        "Emotional Pictures (Selected)/Vigor/P043.bmp",
+        "Emotional Pictures (Selected)/Vigor/P062.bmp",
+        "Emotional Pictures (Selected)/Vigor/P090.bmp",
+        "Emotional Pictures (Selected)/Vigor/P105.bmp",
+        "Emotional Pictures (Selected)/Vigor/Picnic 1.jpg",
+        "Emotional Pictures (Selected)/Vigor/Wedding 2.jpg",
+        "Emotional Pictures (Selected)/Vigor/Wedding 6.jpg",
+        "Emotional Pictures (Selected)/Vigor/Wedding 9.jpg",
+        "Emotional Pictures (Selected)/Vigor/Yoga 4.jpg",
+        "Emotional Pictures (Selected)/Vigor/Yoga 5.jpg"
     ]
 }
 
 let currentCategory = null;
 let currentIndex = 0;
 let repeatCount = 0;
-const maxRepeats = 8; // Number of times to repeat the slideshow for each category
+const maxRepeats = 1; // Number of times to repeat the slideshow for each category
 
 function startSlideShow(category) {
     currentCategory = category;
@@ -113,7 +233,7 @@ function showSlide() {
         setTimeout(() => {
             currentIndex++;
             showSlide();
-        }, 7500); // Display each photo for 7500 ms
+        }, 10000); // Display each photo for 10000 ms
     } else {
         repeatCount++;;
         if (repeatCount < maxRepeats) {
